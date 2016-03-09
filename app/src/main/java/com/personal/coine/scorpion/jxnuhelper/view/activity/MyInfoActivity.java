2cd2f934363623fb7d0677d738d731d9e1f372d7
Coine <793585999@qq.com>
2016-03-09 20:22:38 +0800

    adjust something

---
+++ app/src/main/java/com/personal/coine/scorpion/jxnuhelper/view/activity/MyInfoActivity.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright(c) Runsdata Technologies Co., Ltd.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Runsdata
+ * Technologies Co., Ltd. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the
+ * terms of the license agreement you entered into with Runsdata.
+ * For more information about Runsdata, welcome to http://www.runsdata.com
+ *
+ * Revision History
+ * Date     Version     Name        Description
+ * 2016/3/9  1.0     huangwei    Creation File
+ */
+package com.personal.coine.scorpion.jxnuhelper.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.personal.coine.scorpion.jxnuhelper.R;
+
+/**
+ * Description:
+ *
+ * @author huangwei
+ *         Date 2016/3/9
+ */
+public class MyInfoActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_info);
+        initViews();
+    }
+
+    private void initViews() {
+        findViewById(R.id.row_avadar).setOnClickListener(this);
+        findViewById(R.id.row_username).setOnClickListener(this);
+        findViewById(R.id.row_sex).setOnClickListener(this);
+        findViewById(R.id.row_hometown).setOnClickListener(this);
+        findViewById(R.id.row_personal_sign).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.row_avadar:
+                break;
+            case R.id.row_username:
+                break;
+            case R.id.row_sex:
+                break;
+            case R.id.row_hometown:
+                break;
+            case R.id.row_personal_sign:
+                break;
+            default:
+                break;
+        }
+    }
+}
--- app/src/main/java/com/personal/coine/scorpion/jxnuhelper/view/fragment/MineFragment.java
+++ app/src/main/java/com/personal/coine/scorpion/jxnuhelper/view/fragment/MineFragment.java
@@ -14,6 +14,7 @@
  */
 package com.personal.coine.scorpion.jxnuhelper.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -24,6 +25,7 @@ import android.widget.TextView;
 
 import com.personal.coine.scorpion.jxnuhelper.R;
 import com.personal.coine.scorpion.jxnuhelper.core.ApplicationDelegate;
+import com.personal.coine.scorpion.jxnuhelper.view.activity.MyInfoActivity;
 
 /**
  * Description:
@@ -31,7 +33,7 @@ import com.personal.coine.scorpion.jxnuhelper.core.ApplicationDelegate;
  * @author huangwei
  *         Date 2016/3/9
  */
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -41,7 +43,31 @@ public class MineFragment extends Fragment {
     }
 
     private void initViews(View view) {
-        view.findViewById(R.id.user_avadar_img);
         ((TextView) view.findViewById(R.id.user_name_text)).setText(ApplicationDelegate.getInstance().getCurrentUser().getUsername());
+        view.findViewById(R.id.mine_page_row1).setOnClickListener(this);
+        view.findViewById(R.id.mine_page_row2).setOnClickListener(this);
+        view.findViewById(R.id.mine_page_row3).setOnClickListener(this);
+        view.findViewById(R.id.mine_page_row4).setOnClickListener(this);
+        view.findViewById(R.id.mine_page_row5).setOnClickListener(this);
+        view.findViewById(R.id.mine_page_row6).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mine_page_row1:
+                startActivity(new Intent(getContext(), MyInfoActivity.class));
+                break;
+            case R.id.mine_page_row2:
+                break;
+            case R.id.mine_page_row3:
+                break;
+            case R.id.mine_page_row4:
+                break;
+            case R.id.mine_page_row5:
+                break;
+            case R.id.mine_page_row6:
+                break;
+        }
     }
 }
